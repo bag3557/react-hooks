616698aa8a199194ec099e12bdf166fe6248bb5c
Ajit Bhaik <bag3557@gmail.com>
2020-02-22 17:57:30 +0530

    useEffect - setInterval

--- src/App.js
+++ src/App.js
@@ -1,20 +1,24 @@
 import React from "react";
-import logo from "./logo.svg";
+
 import "./App.css";
 
-import ClassCounter from "./components/ClassCounter";
-import HookCounter from "./components/HookCounter";
-import HookCounterTwo from "./components/HookCounterTwo";
-import HookCounterThree from "./components/HookCounterThree";
-import HookCounterFour from "./components/HookCounterFour";
+// import ClassCounter from "./components/ClassCounter";
+// import HookCounter from "./components/HookCounter";
+// import HookCounterTwo from "./components/HookCounterTwo";
+// import HookCounterThree from "./components/HookCounterThree";
+// import HookCounterFour from "./components/HookCounterFour";
 
-import ClassCounterOne from "./components/ClassCounterOne";
-import HookCounterOne from "./components/HookCounterOne";
+// import ClassCounterOne from "./components/ClassCounterOne";
+// import HookCounterOne from "./components/HookCounterOne";
 
-import ClassMouse from "./components/ClassMouse";
-import HookMouse from "./components/HookMouse";
+// import ClassMouse from "./components/ClassMouse";
+// import HookMouse from "./components/HookMouse";
 
-import MouseContainer from "./components/MouseContainer";
+// import MouseContainer from "./components/MouseContainer";
+
+// import IntervalClassComponent from "./components/IntervalClassComponent";
+import IntervalHookCounter from "./components/IntervalHookCounter";
+
 function App() {
   return (
     <div className="App">
@@ -27,7 +31,9 @@ function App() {
       {/* <HookCounterOne /> */}
       {/* <ClassMouse /> */}
       {/* <HookMouse /> */}
-      <MouseContainer />
+      {/* <MouseContainer /> */}
+      {/* <IntervalClassComponent /> */}
+      <IntervalHookCounter />
     </div>
   );
 }
