b3501426fa6a61c6d4cd5eabb3433a12cd14aed0
Ajit Bhaik <bag3557@gmail.com>
2020-02-22 16:40:46 +0530

    useEffect - for componentDidMount

--- src/App.js
+++ src/App.js
@@ -7,8 +7,12 @@ import HookCounter from "./components/HookCounter";
 import HookCounterTwo from "./components/HookCounterTwo";
 import HookCounterThree from "./components/HookCounterThree";
 import HookCounterFour from "./components/HookCounterFour";
+
 import ClassCounterOne from "./components/ClassCounterOne";
 import HookCounterOne from "./components/HookCounterOne";
+
+import ClassMouse from "./components/ClassMouse";
+import HookMouse from "./components/HookMouse";
 
 function App() {
   return (
@@ -19,7 +23,9 @@ function App() {
       {/* <HookCounterThree /> */}
       {/* <HookCounterFour /> */}
       {/* <ClassCounterOne /> */}
-      <HookCounterOne />
+      {/* <HookCounterOne /> */}
+      {/* <ClassMouse /> */}
+      <HookMouse />
     </div>
   );
 }
